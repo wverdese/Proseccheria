81d8be1211091b62a2692740ec9d1a2ed680094d
wverdese <walt.verdese@gmail.com>
2022-05-29 16:45:06 +0200

    Add crashlytics

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
+    id("com.google.firebase.crashlytics")
     kotlin("android")
 }
 
@@ -64,6 +66,10 @@ dependencies {
     implementation("androidx.compose.material:material:${Version.Compose.MATERIAL}")
     implementation("androidx.compose.ui:ui:${Version.Compose.MAIN}")
     implementation("androidx.compose.ui:ui-tooling:${Version.Compose.MAIN}")
+    /* Firebase */
+    implementation("com.google.firebase:firebase-common-ktx:${Version.Firebase.COMMON_KTX}")
+    implementation("com.google.firebase:firebase-crashlytics-ktx:${Version.Firebase.CRASHLYTICS_KTX}")
+    implementation("com.google.firebase:firebase-analytics-ktx:${Version.Firebase.ANALYTICS_KTX}")
     /* Koin */
     implementation("io.insert-koin:koin-core:${Version.KOIN}")
     implementation("io.insert-koin:koin-androidx-compose:${Version.KOIN}")
@@ -71,6 +77,7 @@ dependencies {
 
 object Version {
     const val KOIN = "3.2.0"
+    const val FIREBASE = "30.1.0"
 
     object AndroidX {
         const val ACTIVITY_COMPOSE = "1.3.1"
@@ -79,5 +86,11 @@ object Version {
     object Compose {
         const val MAIN = "1.1.0"
         const val MATERIAL = "1.2.0-alpha03"
+    }
+
+    object Firebase {
+        const val COMMON_KTX = "20.0.0"
+        const val CRASHLYTICS_KTX = "18.2.11"
+        const val ANALYTICS_KTX = "21.0.0"
     }
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,8 @@ buildscript {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
         classpath("com.android.tools.build:gradle:7.2.0")
         classpath("org.jetbrains.kotlin:kotlin-serialization:1.6.10")
+        classpath("com.google.gms:google-services:4.3.10")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.0")
     }
 }
 
