6c70fdffab0e00d87a334780b78e663fe115754d
wverdese <walt.verdese@gmail.com>
2022-06-26 18:55:40 +0200

    Bump version to 2.0.0 (4)

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "me.wverdese.proseccheria.android"
         minSdk = 24
         targetSdk = 32
-        versionCode = 3
-        versionName = "1.0.1"
+        versionCode = 4
+        versionName = "2.0.0"
     }
     buildTypes {
         getByName("debug") {
