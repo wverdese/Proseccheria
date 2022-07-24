41898fc3a9d3313351547c7e6dff77362d8fd3cd
wverdese <walt.verdese@gmail.com>
2022-07-24 19:04:04 +0200

    Fix FlowSettings provision

--- shared/src/iosMain/kotlin/me/wverdese/proseccheria/iOSKoin.kt
+++ shared/src/iosMain/kotlin/me/wverdese/proseccheria/iOSKoin.kt
@@ -1,12 +1,19 @@
-@file:OptIn(ExperimentalSettingsApi::class)
+@file:OptIn(ExperimentalSettingsApi::class, ExperimentalCoroutinesApi::class)
 
 package me.wverdese.proseccheria
 
+import com.russhwolf.settings.AppleSettings
 import com.russhwolf.settings.ExperimentalSettingsApi
+import com.russhwolf.settings.coroutines.FlowSettings
+import com.russhwolf.settings.coroutines.toFlowSettings
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import me.wverdese.proseccheria.repo.TableDataRepository
 import org.koin.core.Koin
 import org.koin.dsl.module
+import platform.Foundation.NSUserDefaults
 
-actual val platformModule = module {}
+actual val platformModule = module {
+    single { AppleSettings(NSUserDefaults.standardUserDefaults).toFlowSettings() }
+}
 
 fun Koin.getTableDataRepository(): TableDataRepository = get(TableDataRepository::class)
